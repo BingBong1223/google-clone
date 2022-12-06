c6b3a5c31ef7247e98b641eafd8c81d1eaaa0bbb
Leah Westfall <leahwestfall@Leahs-iMac.local>
2022-12-06 12:55:53 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Home from './components/Home';
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SearchPage from './components/SearchPage';
 
 export default function App() {
@@ -9,14 +9,14 @@ export default function App() {
   return (
     <div>
       <Router>
-        <Switch>
-          <Route exact path="./" element={<Home/>}>
+        <Routes>
+          <Route exact path="/" element={<Home/>}>
 
           </Route>
-          <Route path="./search" element={<SearchPage/>}>
+          <Route path="/search" element={<SearchPage/>}>
 
           </Route>
-        </Switch>
+        </Routes>
       </Router>
     </div>
   );
